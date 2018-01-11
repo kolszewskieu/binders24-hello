23c75ea8a6dc0d2bda597975e6c0f5c67214949d
Krzysztof Olszewski <k.olszewski@o2.pl>
2018-01-11 15:27:22 +0100

    initial

--- js/crisp.js
+++ js/crisp.js
@@ -1,1 +1,1 @@
-<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="83e3b31c-1710-46b0-8651-17defcd6fb7e";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="1ed4cf4f-a6ac-4687-871e-2a264e5279d6";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
