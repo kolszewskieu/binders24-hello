9cf988240a80cd28b319fdd53bcce5aef1e7b140
Krzysztof Olszewski <k.olszewski@o2.pl>
2018-01-15 07:43:49 +0100

    binders24

--- js/crisp.js
+++
@@ -1,1 +0,0 @@
-<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="1ed4cf4f-a6ac-4687-871e-2a264e5279d6";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
